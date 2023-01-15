ddd0297868c0d28bb5894456cabcecbbd31915f5
Julian Schacher <julian@jsts.xyz>
2023-01-15 23:35:36 +0100

    Build: Use `gnome-extensions pack` for creating extension zip/bundle

--- package.sh
+++ package.sh
@@ -4,8 +4,11 @@ set -e
 
 REAL_BASE_DIR=$( dirname $( readlink -f "$0" ))
 
-glib-compile-schemas "$REAL_BASE_DIR/src/schemas"
-rm "$REAL_BASE_DIR/top-bar-organizer@julian.gse.jsts.xyz.zip" || true
-cd "$REAL_BASE_DIR/src"
-zip -r "$REAL_BASE_DIR/top-bar-organizer@julian.gse.jsts.xyz.zip" *
-zip -d "$REAL_BASE_DIR/top-bar-organizer@julian.gse.jsts.xyz.zip" "schemas/org.gnome.shell.extensions.top-bar-organizer.gschema.xml"
+gnome-extensions pack "$REAL_BASE_DIR/src" \
+    --force \
+    --extra-source extensionModules \
+    --extra-source prefsModules \
+    --extra-source prefs-box-order-item-row.ui \
+    --extra-source prefs-box-order-list-box.ui \
+    --extra-source prefs-box-order-list-empty-placeholder.ui \
+    --extra-source prefs-page.ui \
